4bbeb2210fb3ff8ed12f920e8aac4730aa782d11
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-09-05 16:36:39 +0200

    Add method and test to debug say hello task

---
+++ src/main/java/_8kyu/debugSayHello/GrassHopper.java
@@ -0,0 +1,10 @@
+package _8kyu.debugSayHello;
+
+public class GrassHopper {
+
+    // https://www.codewars.com/kata/5625618b1fe21ab49f00001f
+
+    public static String sayHello(String name) {
+        return "Hello, " + name;
+    }
+}
