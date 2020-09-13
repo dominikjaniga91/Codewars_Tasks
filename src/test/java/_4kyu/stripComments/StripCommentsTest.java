7774300e8bc19a743bfd087fca9437888af7ba6d
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-09-13 15:25:25 +0200

    Add solution and tests for strip comments task

---
+++ src/test/java/_4kyu/stripComments/StripCommentsTest.java
@@ -0,0 +1,20 @@
+package _4kyu.stripComments;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class StripCommentsTest {
+
+    @Test
+    public void stripComments() {
+        assertEquals(
+                "apples, pears\ngrapes\nbananas",
+                StripComments.stripComments( "apples, pears # and bananas\ngrapes\nbananas !apples", new String[] { "#", "!" } )
+        );
+
+        assertEquals(
+                "a\nc\nd",
+                StripComments.stripComments( "a #b\nc\nd $e f g", new String[] { "#", "$" } )
+        );
+    }
+}
