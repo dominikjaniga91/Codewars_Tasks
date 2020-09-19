d3d3d81f5ec73f41b3fdfbaf02e1ceecfed9debe
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-09-20 00:52:03 +0200

    Add solution and tests for non consecutive number

---
+++ src/test/java/_8kyu/nonConsecutiveNumber/SolutionTest.java
@@ -0,0 +1,16 @@
+package _8kyu.nonConsecutiveNumber;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public final class SolutionTest {
+    @Test public void basicTests() {
+        assertEquals(6, Solution.find(new int[]{1, 2, 3, 4, 6, 7, 8}));
+        assertEquals(-1, Solution.find(new int[]{1, 2, 3, 4, 5, 6, 7, 8}));
+        assertEquals(6, Solution.find(new int[]{4, 6, 7, 8, 9, 11}));
+        assertEquals(11, Solution.find(new int[]{4, 5, 6, 7, 8, 9, 11}));
+        assertEquals(-1, Solution.find(new int[]{31, 32}));
+        assertEquals(0, Solution.find(new int[]{-3, -2, 0, 1}));
+        assertEquals(-1, Solution.find(new int[]{-5, -4, -3, -1}));
+    }
+}
