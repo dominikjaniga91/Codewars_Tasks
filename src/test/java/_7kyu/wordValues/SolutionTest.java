f570a6ff0f2e45f79a6cd58711ba5d14cc3cf06e
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-09-20 20:40:42 +0200

    Change tests in word value task

--- src/test/java/_7kyu/wordValues/SolutionTest.java
+++ src/test/java/_7kyu/wordValues/SolutionTest.java
@@ -2,9 +2,7 @@ package _7kyu.wordValues;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SolutionTest {
 
@@ -13,7 +11,7 @@ public class SolutionTest {
     @DisplayName("Should return integer array with words values")
     void shouldReturnIntArrayWIthWordsValues() {
         assertArrayEquals(new int[]{6,24},Solution.nameValue(new String[]{"abc","abc abc"}));
-//        assertEquals(new int[]{88,12,225},Solution.nameValue(new String[]{"codewars","abc","xyz"}));
-//        assertEquals(new int[]{351,282,330},Solution.nameValue(new String[]{"abcdefghijklmnopqrstuvwxyz","stamford bridge","haskellers"}));
+        assertArrayEquals(new int[]{88,12,225},Solution.nameValue(new String[]{"codewars","abc","xyz"}));
+        assertArrayEquals(new int[]{351,282,330},Solution.nameValue(new String[]{"abcdefghijklmnopqrstuvwxyz","stamford bridge","haskellers"}));
     }
 }
