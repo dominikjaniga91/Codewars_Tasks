364ed73ff9d675ef692bcba33be82d3a66cadac3
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-09-20 12:06:16 +0200

    Add solution and tests for number in string task

---
+++ src/test/java/_7kyu/numbersInString/SolutionTest.java
@@ -0,0 +1,15 @@
+package _7kyu.numbersInString;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+
+    @Test
+    public void basicTests(){
+        assertEquals(9,Solution.solve("2ti9iei7qhr5"));
+        assertEquals(695,Solution.solve("gh12cdy695m1"));
+        assertEquals(85,Solution.solve("lu1j8qbbb85"));
+        assertEquals(185,Solution.solve("185lu1j8qbbb85"));
+    }
+}
