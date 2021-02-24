b533378608b329e6c40b5af869ac732705b012cd
dominikjaniga91 <dominikjaniga91@gmail.com>
2021-02-24 13:02:37 +0100

    Create solution and tests for PositionAverage

---
+++ src/main/java/_6kyu/positionsaverage/PositionAverage.java
@@ -0,0 +1,38 @@
+package _6kyu.positionsaverage;
+
+import java.util.Arrays;
+import static java.util.stream.IntStream.range;
+
+/**
+ * @author Dominik Janiga
+ */
+class PositionAverage {
+    public static double posAverage(String s) {
+        String[] words = s.split(", ");
+        System.out.println("Words " + Arrays.toString(words));
+        int commonPositions = 0;
+
+        for (int i = 0; i < words.length - 1; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                commonPositions += getCommonPositions(words[i], words[j]);
+            }
+        }
+        int combinations = allCombinations(words);
+        return (double) commonPositions / combinations * 100;
+    }
+
+    private static int getCommonPositions(String first, String second) {
+        int commonPositions = 0;
+        for (int i = 0; i < first.length(); i++) {
+            if (first.charAt(i) == second.charAt(i)) {
+                commonPositions++;
+            }
+        }
+        return commonPositions;
+    }
+
+    private static int allCombinations(String[] words) {
+        int sum = range(0, words.length).sum();
+        return sum * words[0].length();
+    }
+}
