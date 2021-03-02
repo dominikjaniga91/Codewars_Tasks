7ef25c1b749a623dac5d0515896bb0d21e916e26
dominikjaniga91 <dominikjaniga91@gmail.com>
2021-03-02 21:40:42 +0100

    Improve tests with DataProvider

--- src/test/java/_7kyu/guessthesequence/GuessTheSequenceTest.java
+++ src/test/java/_7kyu/guessthesequence/GuessTheSequenceTest.java
@@ -1,26 +1,28 @@
 package _7kyu.guessthesequence;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
 
 public class GuessTheSequenceTest {
 
-    @Test
-    public void test0() {
-        int[] result = {1, 10, 11, 12, 13, 14, 15, 16, 2, 3, 4, 5, 6, 7, 8, 9};
-        int x = 16;
-        String expected = Arrays.toString(result),
-                actual = Arrays.toString(GuessTheSequence.sequence(x));
-        Assert.assertEquals(expected, actual);
+    @Test(dataProvider = "sequences")
+    public void sequence_returnIntArrayWithNumbers_InLexicographicalOrder(int numberOfElements, int[] expected) {
+        //when
+        String actual = Arrays.toString(GuessTheSequence.sequence(numberOfElements));
+
+        //then
+        Assert.assertEquals(Arrays.toString(expected), actual);
     }
-    @Test
-    public void test1() {
-        int[] result = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int x = 9;
-        String expected = Arrays.toString(result),
-                actual = Arrays.toString(GuessTheSequence.sequence(x));
-        Assert.assertEquals(expected, actual);
+
+    @DataProvider
+    private Object[][] sequences() {
+        return new Object[][] {
+                {16, new int[]{1, 10, 11, 12, 13, 14, 15, 16, 2, 3, 4, 5, 6, 7, 8, 9}},
+                {9, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}},
+                {21, new int[]{1, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 2, 20, 21, 3, 4, 5, 6, 7, 8, 9}},
+        };
     }
 }
